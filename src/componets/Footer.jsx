906e7d3851a9dfdfae543acd05ad39f667fe86ff
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-22 18:18:39 -0300

    agregamos el footer

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import '../styles/style.css'
-
+import {Link} from 'react-router-dom'
+import logoR from "../img/hoxton_logo.jpg"
 
 const footer = () => {
   return ( 
-  <>
-   <div className='footer__container text-light'>footer</div>
+  <div>
+    <footer className='text-white py-4 bg-dark'>
+      <div className='container'>
+        <nav className='row'>
+          <Link to='/' className='col-12 col-md-3 d-flex aling-items-center justify-content-center'>
+            <img src={logoR} alt="Hoxton" className='mx-2' height='80'/>
+          </Link>
+          <ul className='col-12 col-md-3 list-unstyled'>
+            <li className='font-weight-bold mb-2'>Hoxton</li>
+            <li className='text-center'>En este sitio web podras encontrar los mejores tragos de Rosario</li>
+          </ul>
+          <ul className='col-12 col-md-3 list-unstyled'>
+            <li className='font-weight-bold mb-2'>Enlaces</li>
+            <li>
+              <Link to='/' className='text-reset'>Hoxton</Link>
+            </li>
+            <li>
+              <Link to='/'  className='text-reset'>Productos</Link>
+            </li>
 
-    </>
+          </ul>
+          <ul className='col-12 col-md-3 list-unstyled'>
+            <li className='font-weight-bold mb-2'>Siguenos</li> 
+            <li className='d-flex justify-content-between'>
+              <i class="bi bi-facebook"></i>
+              <i class="bi bi-instagram"></i>
+              <i class="bi bi-twitter"></i>
+            </li>
+          </ul>
+          <div className='container'>
+            <p className='text-center'>Copyright 2022</p>
+          </div>
+        </nav>
+      </div>
+    </footer>
+  </div>
   )
 }
+
+
 
 export default footer
