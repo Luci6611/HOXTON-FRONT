73720493fd3472fe6a24684c5b1ec58d24735857
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-23 20:39:57 -0300

    iniciando admin

--- src/helpers/fetchUsuariosGET.js
+++ src/helpers/fetchUsuariosGET.js
@@ -1,9 +1,9 @@
 const url = "https://hoxton-backend.herokuapp.com/api/usuarios";
 
-export const cargarUsuarios = async (datos) => {
+export const cargarUsuarios = async () => {
   const resp = await fetch(url, {
     method: "GET",
-    body: JSON.stringify(datos),
+    body: JSON.stringify(),
     headers: {
       "Content-type": "application/json; charset=UTF-8",
     },
