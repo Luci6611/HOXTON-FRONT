7bd8b109135119bc6c2e0089a4580bd7ba9e50e7
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-09-06 16:17:20 -0300

    vamos agregando pedidos para completar

--- src/Pages/Pedidos.jsx
+++ src/Pages/Pedidos.jsx
@@ -43,26 +43,27 @@ const Pedidos = () => {
     <link rel="shortcut icon" href={favicon} type="image/x-icon" />
     <title>Pedidos</title>
   </Helmet>
-    <div className="container my-5">
+    <div className="container m-0 d-flex flex-column justify-content-center align-items-center ">
+        <h1 className="pedidos__titulo text-center">Pedidos</h1>
     <div className="row">
       <div className="col">
         <hr />
       </div>
     </div>
-    <div className="row row-cols-1 row-cols-md-3 g-4 mb-2">
+    <div className="row d-flex  row-cols-1 row-cols-md-3 g-4 mb-2">
       {pedidos.map((pedido) => (
         <div className="col" key={pedido._id}>
           <div className="card h-100">
             
-            <div className="card-body d-flex flex-column    ">
+            <div className="card-body d-flex flex-column cuerpo_pedido    ">
               
-              <h5 className="card-title nombre-menu ">{pedido.menu.nombre}</h5>
+              <h5 className="card-title nombre-menu__pedido ">{pedido.menu.nombre}</h5>
             
-              <p className="card-text ">{pedido.usuario.nombre}</p>
-              <p className="card-text "> identificacion:{pedido.usuario.userId}</p>
+              <p className="card-text usuario__pedido">Cliente: {pedido.usuario.nombre}</p>
+              <p className="card-text  usuario__pedido "> identificacion:{pedido.usuario.userId}</p>
 
-              <p className="card-text ">{Date(pedido.fecha)}</p>
-              <p className="card-text ">${pedido.menu.precio}</p>
+              <p className="card-text  usuario__pedido">Horario:{Date(pedido.fecha)}</p>
+              <p className="card-text  usuario__pedido ">Precio: ${pedido.menu.precio}</p>
               
               
               <span className="badge mb-3 p-2  rounded-pill bg-danger precio">
@@ -76,7 +77,7 @@ const Pedidos = () => {
       ))}
     </div>
     <div className="row">
-      <div className="col">
+      <div className="col d-flex justify-content-center">
         <BtnPagination
           registro={registro}
           total={total}
