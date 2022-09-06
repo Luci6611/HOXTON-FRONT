b9fb9190a11c3224bb4ba5631ed8feb06046eede
CarlosMMajek <carlosestemolina@hotmail.com>
2022-09-06 17:09:30 -0300

    confirmar contraseñas completo

--- src/Pages/Registro.jsx
+++ src/Pages/Registro.jsx
@@ -1,51 +1,63 @@
-import React, { useState } from "react";
+import React, { useState,useRef ,useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { crearUsuario } from "../helpers/fetchUsuarios";
 import "../styles/Registro.css"
 
 const Registro = () => {
   const navigate = useNavigate();
-
+  const inputRef = useRef(null)
   const [formValues, setFormValues] = useState({
     nombre: "",
     email: "",
     password: "",
+    confirmpass:"",
     role: "USER_ROLE",
   });
 
   const [message, setMessage] = useState([]);
 
+  useEffect(() => {},[inputRef])
+  
+
   const handleChange = ({ target }) => {
-    setFormValues({
-      ...formValues,
-      [target.name]: target.value,
-    });
+    let name = target.name;
+    let value = target.value;
+    
+    setFormValues((prev) => {return {...prev,[name]:value}   } );
+    
   };
 
   const guardarDatos = async (e) => {
     e.preventDefault();
+    inputRef.current.setCustomValidity("")
+  
+    if(formValues.password === formValues.confirmpass){
+      
+      const respuesta = await crearUsuario(formValues);
 
-    const respuesta = await crearUsuario(formValues);
-
-    if (respuesta?.usuario) {
-      setMessage([{ ok: true, msg: respuesta.msg }]);
-      setTimeout(() => {
-        setMessage([]);
-      }, 3000);
-    } else {
-      if (respuesta?.errors) {
-        setMessage(respuesta.errors);
+      if (respuesta?.usuario) {
+        setMessage([{ ok: true, msg: respuesta.msg }]);
+        setTimeout(() => {
+          setMessage([]);
+        }, 3000);
       } else {
-        setMessage([{ msg: respuesta.msg }]);
+        if (respuesta?.errors) {
+          setMessage(respuesta.errors);
+        } else {
+          setMessage([{ msg: respuesta.msg }]);
+        }
       }
-    }
-
-    setFormValues({
-      nombre: "",
+  
+      setFormValues({nombre: "",
       email: "",
       password: "",
-      role: "USER_ROLE",
-    });
+      confirmpass:"",
+      role: "USER_ROLE",})
+      
+    }else{
+      inputRef.current.setCustomValidity("Las contraseñas deben coincidir")
+    }
+    
 
     // console.log(respuesta);
   };
@@ -66,7 +78,7 @@ const Registro = () => {
                   className="form-control mb-2 input"
                   type="text"
                   placeholder="Ingrese su nombre o usuario"
-                  name="nombre"
+                  name="nombre" maxLength={12} minLength={4}
                   value={formValues.nombre}
                   onChange={handleChange}
                   autoFocus={true}
@@ -74,7 +86,7 @@ const Registro = () => {
                 />
                 <input
                   className="form-control mb-2 input"
-                  type="email"
+                  type="email" maxLength={40}
                   placeholder="Ingrese su email"
                   name="email"
                   value={formValues.email}
@@ -83,17 +95,20 @@ const Registro = () => {
                 <input
                   className="form-control input mt-1"
                   type="password"
-                  placeholder="ingrese contraseña"
-                  name="password"
+                  placeholder="Ingrese contraseña"
+                  name="password" maxLength={15} minLength={5}
                   value={formValues.password}
+                  id="contraseña"
                   onChange={handleChange}
                 />
                   <input
+                  ref={inputRef}
+                  required
                   className="form-control input"
                   type="password"
-                  placeholder="confirmar contraseña"
-                  name="password"
-                  value={formValues.password}
+                  placeholder="Confirmar contraseña"
+                  name="confirmpass"
+                  id="confirmar_contraseña"
                   onChange={handleChange}
                 />
 
